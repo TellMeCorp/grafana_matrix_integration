9a2a34276125e19bf7ae591135da3e09cccb8b1a
wirdes <45904810+wirdes@users.noreply.github.com>
2022-10-13 16:32:20 +0300

    hotfix

--- routers/index.js
+++ routers/index.js
@@ -5,11 +5,12 @@ const fs = require("fs");
 const path = require("path");
 const ks = fs.readFileSync(path.join(__dirname, "../src/Keys.json"));
 const { register, login, tokenVerify, loguot } = require("../controllers/jwt");
+const asyncHandler = require("express-async-handler");
 
-router.get("/jwks", async (req, res) => {
+router.get("/jwks", asyncHandler((async (req, res) => {
   const keyStore = await jose.JWK.asKeyStore(ks.toString());
   res.send(keyStore.toJSON());
-});
+}));
 
 router.post("/login", login);
 router.post("/verify", tokenVerify);
