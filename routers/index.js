c7190509f89435742b0daeddabe7a193ba93b1d2
wirdes <45904810+wirdes@users.noreply.github.com>
2022-10-13 11:13:50 +0300

    delete unusage lines

--- routers/index.js
+++ routers/index.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { register, login, tokenVerify, loguot } = require("../controllers/jwt");
 
-// router.get("/", (req, res, next) => {
-//   res.send("/login , /tokenVerify");
-// });
-
 router.post("/login", login);
 
 router.post("/verify", tokenVerify);
