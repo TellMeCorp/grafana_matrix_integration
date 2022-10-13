59c92669fa7ba4979333b7debb55bad7961f08c1
wirdes <45904810+wirdes@users.noreply.github.com>
2022-10-13 16:14:09 +0300

    hotfix

--- routers/index.js
+++ routers/index.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const jose = require("node-jose");
-const fs = require("fs");
-const path = require("path");
-const ks = fs.readFileSync(path.join(__dirname, "../src/Keys.json"));
 const { register, login, tokenVerify, loguot } = require("../controllers/jwt");
 
 router.get("/jwks", async (req, res) => {
+  const jose = require("node-jose");
+  const fs = require("fs");
+  const path = require("path");
+  const ks = fs.readFileSync(path.join(__dirname, "../src/Keys.json"));
   const keyStore = await jose.JWK.asKeyStore(ks.toString());
   res.send(keyStore.toJSON());
 });
 
 router.post("/login", login);
 router.post("/verify", tokenVerify);
-router.post("/logout", logout);
 
 module.exports = router;
