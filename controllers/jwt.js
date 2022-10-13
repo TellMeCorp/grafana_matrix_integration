48223978ad56ded668c8f34028f128e70d984ef5
wirdes <45904810+wirdes@users.noreply.github.com>
2022-10-13 11:14:16 +0300

    Add login check per admin

--- controllers/jwt.js
+++ controllers/jwt.js
@@ -1,42 +1,42 @@
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 const axios = require("axios");
-
 const CustomError = require("../helpers/errors/CustomError");
-
 const {
   isTokenIncluded,
   getAccessTokenFromHeader,
   sendJwtToClient,
 } = require("../helpers/auth/tokenHelpers");
-const { generateJwtFromUser } = require("../middlewares/auth/jwt");
 
 const login = asyncHandler(async (req, res, next) => {
-  const { name, homeserver } = req.body;
-  const r = await axios.get(
-    `${homeserver}/_synapse/admin/v1/users/${name}/admin`
+  const { name, homeserver, accessToken } = req.body;
+  var r = await axios.get(
+    `${homeserver}_synapse/admin/v1/users/${name}/admin`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
   );
-  res.send(r);
-  sendJwtToClient(name, res);
+  if (r.data["admin"] == true) {
+    sendJwtToClient(name, res);
+  } else {
+    return next(
+      new CustomError("Admin girişi yapmadan burayı göremezsin", 401)
+    );
+  }
 });
 
-const tokenVerify = (req, res) => {
+const tokenVerify = (req, res, next) => {
   const { JWT_SECRET } = process.env;
   const accessToken = req.body.token || req.query.token;
   if (!accessToken) {
-    return res.status(400).json({
-      success: false,
-      message: "token yanlış",
-    });
+    return next(new CustomError("Hatalı Token", 401));
   }
   jwt.verify(accessToken, JWT_SECRET, async (err, decoded) => {
     if (err) {
-      return res.status(401).json({
-        success: false,
-        message: "token yanlış",
-      });
+      return next(new CustomError("Hatalı Token", 401));
     }
-
     res.status(200).json({
       success: true,
       data: { name: decoded.name, token: accessToken },
@@ -52,6 +52,7 @@ const loguot = (req, res) => {
   jwt.destroy(accessToken);
   res.status(200).json({
     success: true,
+    message: "Çıkış yapıldı",
   });
 };
 
