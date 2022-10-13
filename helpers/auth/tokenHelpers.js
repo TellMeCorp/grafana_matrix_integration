c3871040cd8bc500c696f2c0138f78c3522ee5b1
wirdes <45904810+wirdes@users.noreply.github.com>
2022-10-13 15:52:20 +0300

    fix import

--- helpers/auth/tokenHelpers.js
+++ helpers/auth/tokenHelpers.js
@@ -1,7 +1,10 @@
 const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const ms = require("ms");
+const jose = require("node-jose");
 
-const sendJwtToClient = (name, response) => {
-  const token = generateJwtFromUser(name);
+const sendJwtToClient = async (name, response) => {
+  const token = await generateJwtFromUser(name);
   const { JWT_COOKIE, NODE_ENV } = process.env;
   return response.status(200).json({
     success: true,
@@ -21,18 +24,24 @@ const getAccessTokenFromHeader = (req) => {
   const access_token = authorization.split(" ")[1];
   return access_token;
 };
-const generateJwtFromUser = (name) => {
-  const { JWT_EXPIRE, JWT_SECRET } = process.env;
+const generateJwtFromUser = async (name) => {
+  const path = require("path");
+  const JWKeys = fs.readFileSync(path.join(__dirname, "../../src/Keys.json"));
 
-  const token = jwt.sign(
-    {
-      name,
-    },
-    JWT_SECRET,
-    {
-      expiresIn: JWT_EXPIRE,
-    }
-  );
+  const keyStore = await jose.JWK.asKeyStore(JWKeys.toString());
+
+  const [key] = keyStore.all({ use: "sig" });
+
+  const opt = { compact: true, jwk: key, fields: { typ: "jwt" } };
+
+  const payload = JSON.stringify({
+    name,
+    exp: Math.floor((Date.now() + ms("1d")) / 1000),
+    iat: Math.floor(Date.now() / 1000),
+    sub: "test",
+  });
+
+  const token = await jose.JWS.createSign(opt, key).update(payload).final();
   return token;
 };
 
